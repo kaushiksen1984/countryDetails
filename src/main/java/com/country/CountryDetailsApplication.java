ce89249bf66078526bfff8f9bda061d75876b605
Sen <kaushik.sen@capgemini.com>
2020-12-03 22:59:53 -0600

    Fixing Jenkins Pipeline

--- src/main/java/com/country/CountryDetailsApplication.java
+++ src/main/java/com/country/CountryDetailsApplication.java
@@ -2,12 +2,19 @@ package com.country;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CountryDetailsApplication {
+public class CountryDetailsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CountryDetailsApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CountryDetailsApplication.class);
 	}
 
 }
